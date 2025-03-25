476a4eff3c4be98cdb3054f075bdf0be0a6aa99f
EliasPereirah <16616409+EliasPereirah@users.noreply.github.com>
2025-03-25 17:00:23 -0300

    Adding Google model: gemini-2.5-pro-exp-03-25

--- js/script.js
+++ js/script.js
@@ -56,6 +56,7 @@ let PLATFORM_DATA = {
     },
     google: {
         models: [
+            "gemini-2.5-pro-exp-03-25",
             "gemini-2.0-pro-exp-02-05",
             "gemini-1.5-pro",
             "gemini-2.0-flash",
