2822690b15d1a54da19d68fa6959eaa9034f33ed
EliasPereirah <16616409+EliasPereirah@users.noreply.github.com>
2025-05-20 20:23:06 -0300

    Removing model: gemma-3n-e4b-it

--- js/script.js
+++ js/script.js
@@ -59,8 +59,7 @@ let PLATFORM_DATA = {
         models: [
             "gemini-2.5-pro-preview-05-06",
             "gemini-2.5-flash-preview-05-20",
-            "gemini-1.5-pro",
-            "gemma-3n-e4b-it"
+            "gemini-1.5-pro"
         ],
         name: "Google",
         endpoint: 'https://generativelanguage.googleapis.com/v1beta/models/{{model}}:{{gen_mode}}?key={{api_key}}'
